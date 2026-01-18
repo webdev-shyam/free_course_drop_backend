6cec54c8387d96941a0214c95f56059ccaba5b5c
Ganeshyam <zoriceman@gmail.com>
2026-01-18 13:45:43 +0630

    mongoURI updated

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import os
 
-MONGO_URL = os.getenv("MONGO_URL")  # Add your connection string in Render environment
+MONGO_URL = os.getenv("MONGO_URI")  # Add your connection string in Render environment
 client = MongoClient(MONGO_URL)
 db = client["udemy_courses_db"]
 courses_collection = db["courses"]
