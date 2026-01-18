c3b5a1cccea962a3470031dad0473dd59724617c
Ganeshyam <zoriceman@gmail.com>
2026-01-18 15:22:33 +0630

    updated scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from app.scraper import get_udemy_courses
+from app.scraper import scrape_discudemy
 from app.telegram_bot import post_to_telegram
 import logging
 import pytz
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 scheduler = BlockingScheduler(timezone=pytz.UTC)
 
 # Scraper every 4 hours
-scheduler.add_job(get_udemy_courses, 'interval', hours=4, id="udemy_scraper")
+scheduler.add_job(scrape_discudemy, 'interval', hours=4, id="udemy_scraper")
 
 # Telegram posting every 2.5 hours
 scheduler.add_job(post_to_telegram, 'interval', hours=2, minutes=30, id="telegram_poster")
